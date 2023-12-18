aac0015b07a7172f47c4dbf5ecbe520c8c2d1afa
omar <yourname@example.com>
2023-12-18 13:16:04 +0300

    initial commit

---
+++ code.js
@@ -0,0 +1,8 @@
+const rows = document.createElement('div');
+const columns = document.createElement('div');
+const container = document.querySelector('.container');
+
+container.appendChild(columns);
+container.appendChild(rows);
+
+console.log(container);
